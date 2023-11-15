66f4d90fd535c39e54c56b44ce62fb4d673f56ba
Ashish Ahuja <ashishahuja124@gmail.com>
2023-11-15 12:05:54 +0530

    SignIn and Signup form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,14 @@
+import {useState} from "react";
 import Header from "./Header";
 
 const Login = () => {
+
+    const [isSignInForm, setIsSignInForm] = useState(true);
+
+    const toggleSignInForm = () => {
+        setIsSignInForm(!isSignInForm);
+    }
+
     return (
         <div>
             <Header />
@@ -12,8 +20,17 @@ const Login = () => {
             <form className="bg-black absolute p-12 w-4/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"  >
                 <div>
                     <h1 className="text-2xl py-4">
-                        Sign In
+                        {isSignInForm ? "Sign In" : "Sign Up"}
                     </h1>
+
+                    {!isSignInForm && (
+                        <input type="text"
+                            placeholder="Full Name"
+                            className="p-4 my-4 w-full bg-gray-700">
+                        </input>
+                    )}
+
+                    
                     <input type="email"
                         placeholder="Enter your Email"
                         className="p-4 my-4 w-full bg-gray-700">
@@ -23,10 +40,10 @@ const Login = () => {
                         className="p-4 my-4 w-full bg-gray-700">
                     </input>
                     <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
-                        Sign In
+                        {isSignInForm ? "Sign In" : "Sign Up"}
                     </button>
-                    <p className="py-2 my-2">
-                        New to Netlix?..Sign Up Now
+                    <p className="py-2 cursor-pointer" onClick={toggleSignInForm}>
+                        {isSignInForm ? "New to Netlix?..Sign Up Now" : "Already a User? Sign In Now"}
                     </p>
                 </div>
 
